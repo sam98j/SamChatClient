3b92e7ee8adf4fb97ec2b72a8ebc6bedea4dfe41
HosamAlden <hosam98j@gmail.com>
2023-08-01 08:44:28 +0200

    fix font issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,22 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { appWithTranslation } from "next-i18next";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
+import localFont from "next/font/local";
+// import the lcoal font
+const effraFont = localFont({
+  src: "../../public/efrraweb/alfont_com_alfont_com_effra_md-1-webfont.woff2",
+  display: "swap",
+  // @ts-ignore
+  subsets: ["arabic"],
+});
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Provider store={store}>
-        <Component {...pageProps} />
+        <div className={effraFont.className}>
+          <Component {...pageProps} />
+        </div>
       </Provider>
     </ChakraProvider>
   );
