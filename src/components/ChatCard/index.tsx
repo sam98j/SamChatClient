c4c57c2a6d4f7d2c462493d0fd912cf057566abc
HosamAlden <hosam98j@gmail.com>
2024-05-18 15:33:15 +0200

    change font family in chatcard

--- src/components/ChatCard/index.tsx
+++ src/components/ChatCard/index.tsx
@@ -103,7 +103,7 @@ const ChatCard: React.FC<{ chat: SingleChat }> = ({ chat }) => {
         <Avatar name='Hosam Alden' src={chatAvatar} />
         <Box flexGrow={'1'}>
           {/* chat usr name */}
-          <Heading size={'sm'} marginBottom={'5px'} textColor={'messenger.500'}>
+          <Heading size={'sm'} marginBottom={'5px'} textColor={'messenger.500'} fontFamily={'inherit'}>
             {chat.usrname}
           </Heading>
           {/* usr actions (usr typing, recording voice) */}
