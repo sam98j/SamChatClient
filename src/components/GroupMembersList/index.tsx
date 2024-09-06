d48188ca8ceaebf4e62c6eb398b1d9f813d7b4e4
HosamAlden <hosam98j@gmail.com>
2024-09-06 07:46:20 +0200

    add translation to add_groub_member_btn

--- src/components/GroupMembersList/index.tsx
+++ src/components/GroupMembersList/index.tsx
@@ -3,16 +3,20 @@ import { Box, Button, List } from '@chakra-ui/react';
 import React, { FC } from 'react';
 import { BsPersonAdd } from 'react-icons/bs';
 import GroupMember from '../GroupMember';
+import useTranslation from 'next-translate/useTranslation';
 
 // props
 type Props = { members: ChatMember[] };
 
 const GroupMembersList: FC<Props> = ({ members }) => {
+  // translation method
+  const { t } = useTranslation('chatProfile');
+
   return (
     <Box backgroundColor={'gray.100'} width={'full'} padding={'10px'}>
       <Button display={'flex'} gap={'10px'} padding={0}>
         <BsPersonAdd size={'1.5rem'} />
-        Add Member
+        {t('group_chat.add_members_btn')}
       </Button>
       {/* loop throw members */}
       <List display={'flex'} flexDirection={'column'} gap={'10px'}>
