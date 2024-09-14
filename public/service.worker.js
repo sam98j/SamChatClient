c784572fb0bc8a1b7f3dd21a0dafe49c58999fb2
HosamAlden <hosam98j@gmail.com>
2024-09-14 09:31:23 +0200

    test notification

--- public/service.worker.js
+++ public/service.worker.js
@@ -1,5 +1,5 @@
 // const NEXT_PUBLIC_API_URL = 'https://api.chat.samapps.xyz';
-// const NEXT_PUBLIC_API_URL = 'http://192.168.37.78:2000';
+// const NEXT_PUBLIC_API_URL = 'http://192.168.231.78:2000';
 const NEXT_PUBLIC_API_URL = 'https://samchat.onrender.com';
 // caches name
 const cacheName = 'v1';
@@ -23,7 +23,10 @@ const urlBase64ToUnit8Array = (base64String) => {
 const saveSubscription = async (access_token, subscription) => {
   const response = await fetch(`${API_URl}/users/save-subscription`, {
     method: 'post',
-    headers: { 'Content-type': 'application/json', authorization: access_token },
+    headers: {
+      'Content-type': 'application/json',
+      authorization: access_token,
+    },
     body: JSON.stringify(subscription),
   });
   return response.json();
@@ -45,17 +48,18 @@ self.addEventListener('activate', async () => {
 self.addEventListener('push', (e) => {
   console.log('push');
   const { senderImg, senderName, msgText } = e.data.json();
-  self.registration.showNotification(senderName, {
-    body: msgText,
-    icon: `${NEXT_PUBLIC_API_URL}${senderImg}`,
-  });
+  // self.registration.showNotification(senderName, {
+  //   body: msgText,
+  //   icon: `${NEXT_PUBLIC_API_URL}${senderImg}`,
+  // });
 });
 
 self.addEventListener('message', async (e) => {
   if (e.data.action !== 'access_token') return;
   // jwt token
   const access_token = e.data.value;
-  const PUBLIC_VAPID_KEY = 'BLtXuyohy-TNHkRgrHWMmNISkuO4p4yvHMViO4zPfuaH1RsAxboqRjQVm7XnbWGAJw5ovjNuuWOyjvzhFN86EEE';
+  const PUBLIC_VAPID_KEY =
+    'BLtXuyohy-TNHkRgrHWMmNISkuO4p4yvHMViO4zPfuaH1RsAxboqRjQVm7XnbWGAJw5ovjNuuWOyjvzhFN86EEE';
   const subscription = await self.registration.pushManager.subscribe({
     userVisibleOnly: true,
     applicationServerKey: urlBase64ToUnit8Array(PUBLIC_VAPID_KEY),
