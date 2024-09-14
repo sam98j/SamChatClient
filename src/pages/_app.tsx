d4006594a92dc1ccf6081e819e9c5d2302b0bdd9
HosamAlden <hosam98j@gmail.com>
2024-09-14 09:31:23 +0200

    test notification

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -181,6 +181,9 @@ function App({ Component, ...pageProps }: AppProps) {
     socketClient?.removeAllListeners('message');
     // listen for new message
     socketClient?.on('message', (message: ChatMessage) => {
+      const notification = new Notification('notification', {
+        body: 'notfication body',
+      });
       // place last updated chat to the top
       dispatch(placeLastUpdatedChatToTheTop({ chatId: message.receiverId }));
       // set chat's last message
