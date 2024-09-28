2d5093296ab982a97edb76d4eb5e7702bfd7c79c
HosamAlden <hosam98j@gmail.com>
2024-09-28 09:14:07 +0200

    refactoring app modals

--- src/components/CreateChat/index.tsx
+++ src/components/CreateChat/index.tsx
@@ -1,17 +1,27 @@
 /* eslint-disable react/no-unknown-property */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import NewChatUser from '../NewChatUser';
 import useUsersApi from './getUsrs.hook';
 import { LoggedInUserData } from '@/redux/auth.slice';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/redux/store';
 import { useDispatch } from 'react-redux';
-import { changeNewChatScrStatus, setVisablityOfCreateChatGroupMenu } from '@/redux/system.slice';
+import {
+  changeNewChatScrStatus,
+  setVisablityOfCreateChatGroupMenu,
+} from '@/redux/system.slice';
 import SearchInput from '../SearchInput/SearchInput';
 import { Button, Text } from '@chakra-ui/react';
 import { MdOutlineGroups2 } from 'react-icons/md';
 import useTranslation from 'next-translate/useTranslation';
-import { Drawer, DrawerContent } from '../ui/drawer';
+import { Drawer } from 'vaul';
+
+// component state
+type State = {
+  searchqr: string;
+  fetchedUsers: Omit<LoggedInUserData, 'email'>[];
+  loading: boolean;
+};
 
 const CreateChat = () => {
   // translation method
@@ -19,19 +29,26 @@ const CreateChat = () => {
   // redux store dispatch function
   const dispatch = useDispatch();
   // redux store
-  const isModalOpen = useSelector((state: RootState) => state.system.isNewChatScreenOpen);
+  const isModalOpen = useSelector(
+    (state: RootState) => state.system.isNewChatScreenOpen,
+  );
   const { fetchUsers } = useUsersApi();
   // use effect
   // componet state
-  const [state, setState] = useState<{
-    searchqr: string;
-    fetchedUsers: Omit<LoggedInUserData, 'email'>[];
-    loading: boolean;
-  }>({
+  const [state, setState] = useState<State>({
     searchqr: '',
     fetchedUsers: [],
     loading: false,
   });
+  useEffect(() => {
+    if (!isModalOpen) {
+      document.body.style.margin = '0px';
+      document.body.style.borderRadius = '0px';
+      return;
+    }
+    document.body.style.setProperty('margin', '10px', 'important');
+    document.body.style.borderRadius = '10px';
+  }, [isModalOpen]);
   // handleFormChange
   const handleFormChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.value) {
@@ -54,32 +71,47 @@ const CreateChat = () => {
     dispatch(changeNewChatScrStatus(false));
   };
   return (
-    <Drawer open={isModalOpen} onClose={() => dispatch(changeNewChatScrStatus(false))}>
-      <DrawerContent className='p-2 h-4/5 flex flex-col gap-3 outline-none'>
-        <SearchInput data={{ handleFormChange, loadingState: state.loading }} />
-        <Button
-          onClick={createGroupChatBtnHandler}
-          width={'full'}
-          borderRadius={'2xl'}
-          colorScheme='blue'
-          paddingTop={'10px'}
-          paddingBottom={'10px'}
-          display={'flex'}
-          alignItems={'center'}
-          gap={'10px'}
-          justifyContent={'flex-start'}
-        >
-          <MdOutlineGroups2 size={'1.5rem'} color='white' />
-          <Text>{t('createNewChatGroupBtn')}</Text>
-        </Button>
-        {/* user */}
-        <div className='overflow-y-scroll'>
-          {state.fetchedUsers.map((usr) => (
-            <NewChatUser key={usr._id} usr={usr} searchqr={state.searchqr} />
-          ))}
-        </div>
-      </DrawerContent>
-    </Drawer>
+    <Drawer.Root
+      open={isModalOpen}
+      onClose={() => dispatch(changeNewChatScrStatus(false))}
+    >
+      <Drawer.Portal>
+        <Drawer.Overlay className="fixed inset-0 bg-black/40" />
+        <Drawer.Content className="bg-gray-100 h-[95dvh] fixed bottom-0 left-0 right-0 outline-none rounded-t-xl">
+          <div className="p-4 bg-white h-full rounded-t-xl flex flex-col gap-2">
+            <Drawer.Handle />
+            <SearchInput
+              data={{ handleFormChange, loadingState: state.loading }}
+            />
+            <Button
+              onClick={createGroupChatBtnHandler}
+              width={'full'}
+              borderRadius={'2xl'}
+              colorScheme="blue"
+              paddingTop={'10px'}
+              paddingBottom={'10px'}
+              display={'flex'}
+              alignItems={'center'}
+              gap={'10px'}
+              justifyContent={'flex-start'}
+            >
+              <MdOutlineGroups2 size={'1.5rem'} color="white" />
+              <Text>{t('createNewChatGroupBtn')}</Text>
+            </Button>
+            {/* user */}
+            <div className="overflow-y-scroll">
+              {state.fetchedUsers.map((usr) => (
+                <NewChatUser
+                  key={usr._id}
+                  usr={usr}
+                  searchqr={state.searchqr}
+                />
+              ))}
+            </div>
+          </div>
+        </Drawer.Content>
+      </Drawer.Portal>
+    </Drawer.Root>
   );
 };
 
--- src/components/CreateChatGroupMenu/index.tsx
+++ src/components/CreateChatGroupMenu/index.tsx
@@ -5,17 +5,26 @@ import { useSelector } from 'react-redux';
 import GroupMemberCard from '../GroupMemberCard';
 import SearchInput from '../SearchInput/SearchInput';
 import styles from './styles.module.scss';
-import { ChatMember, ChatTypes, SingleChat, setOpenedChat } from '@/redux/chats.slice';
+import {
+  ChatMember,
+  ChatTypes,
+  SingleChat,
+  setOpenedChat,
+} from '@/redux/chats.slice';
 import { useDispatch } from 'react-redux';
 import { addChatMembers, createChat } from '@/apis/chats.api';
 import { AnyAction } from '@reduxjs/toolkit';
 import { v4 } from 'uuid';
 import { useRouter, useSearchParams } from 'next/navigation';
-import { setSystemNotification, setVisablityOfCreateChatGroupMenu } from '@/redux/system.slice';
+import {
+  setSystemNotification,
+  setVisablityOfCreateChatGroupMenu,
+} from '@/redux/system.slice';
 import useTranslation from 'next-translate/useTranslation';
 import { CiCamera } from 'react-icons/ci';
-import { Drawer, DrawerContent } from '../ui/drawer';
+import { Drawer } from 'vaul';
 
+// components props
 type GroupData = {
   name: string;
   avatar: File;
@@ -33,18 +42,25 @@ const CreateChatGroupMenu: FC<{ forCreation: boolean }> = ({ forCreation }) => {
   // params
   const params = useSearchParams();
   // get usr chat from redux store
-  const { chats, addChatMembersRes } = useSelector((state: RootState) => {
-    return {
-      chats: state.chat.chats?.filter((chat) => chat.type === ChatTypes.INDIVISUAL),
-      addChatMembersRes: state.chat.addChatMembersRes,
-    };
-  });
+  const { chats, isCreateChatGroupMenuOpen, addChatMembersRes } = useSelector(
+    (state: RootState) => {
+      return {
+        chats: state.chat.chats?.filter(
+          (chat) => chat.type === ChatTypes.INDIVISUAL,
+        ),
+        addChatMembersRes: state.chat.addChatMembersRes,
+        isCreateChatGroupMenuOpen: state.system.isCreateChatGroupMenuOpen,
+      };
+    },
+  );
   // selected members
   const [selectedMembers, setSelectedMembers] = useState<ChatMember[]>([]);
   // groupName
   const [groupData, setGroupData] = useState<GroupData>(null!);
   // current usr id
-  const loggedInUser = useSelector((state: RootState) => state.auth.currentUser) as ChatMember;
+  const loggedInUser = useSelector(
+    (state: RootState) => state.auth.currentUser as ChatMember,
+  );
   // members selection complete
   const [isMemberSelectionDone, setIsMemberSelectionDone] = useState(false);
   // add members/continue btn
@@ -56,7 +72,8 @@ const CreateChatGroupMenu: FC<{ forCreation: boolean }> = ({ forCreation }) => {
   // inputChangeHandler
   const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     // get chat avatar
-    if (e.target.files) return setGroupData({ ...groupData, avatar: e.target.files![0] });
+    if (e.target.files)
+      return setGroupData({ ...groupData, avatar: e.target.files![0] });
     // get other data
     setGroupData({ ...groupData, [e.target.name]: e.target.value });
   };
@@ -71,7 +88,12 @@ const CreateChatGroupMenu: FC<{ forCreation: boolean }> = ({ forCreation }) => {
         name: groupData.name,
         avatar: '',
       };
-      dispatch(createChat({ chat: createdChat, avatar: groupData.avatar }) as unknown as AnyAction);
+      dispatch(
+        createChat({
+          chat: createdChat,
+          avatar: groupData.avatar,
+        }) as unknown as AnyAction,
+      );
       dispatch(setVisablityOfCreateChatGroupMenu(false));
       dispatch(setOpenedChat(createdChat));
       push('/chat?id=' + createdChat._id);
@@ -82,7 +104,12 @@ const CreateChatGroupMenu: FC<{ forCreation: boolean }> = ({ forCreation }) => {
       // set isLoading
       setIsLoading(true);
       // add members
-      dispatch(addChatMembers({ chatId: params.get('id')!, members: selectedMembers! }) as unknown as AnyAction);
+      dispatch(
+        addChatMembers({
+          chatId: params.get('id')!,
+          members: selectedMembers!,
+        }) as unknown as AnyAction,
+      );
     }
     // terminate if no members selected
     if (selectedMembers.length === 0) return;
@@ -95,62 +122,99 @@ const CreateChatGroupMenu: FC<{ forCreation: boolean }> = ({ forCreation }) => {
     if (!addChatMembersRes) return;
     dispatch(setVisablityOfCreateChatGroupMenu(false));
     // show system notification
-    dispatch(setSystemNotification({ err: false, msg: t('membersAdditionStatus') }));
+    dispatch(
+      setSystemNotification({ err: false, msg: t('membersAdditionStatus') }),
+    );
   }, [addChatMembersRes]);
+  // coponent mount
+  useEffect(() => {
+    document.body.style.setProperty('margin', '10px', 'important');
+    document.body.style.borderRadius = '10px';
+    // componet unmount
+    return function cleanUp() {
+      document.body.style.setProperty('margin', '0px', 'important');
+      document.body.style.borderRadius = '0px';
+      return;
+    };
+  }, []);
+  // modal open observer
+  useEffect(() => {}, [isCreateChatGroupMenuOpen]);
   return (
-    <Drawer open={true} dismissible={true} onClose={() => dispatch(setVisablityOfCreateChatGroupMenu(false))}>
-      <DrawerContent className={`${styles.createChatGroupMenu} p-2 outline-none`}>
-        <div className='mt-2'>
-          <SearchInput data={{ handleFormChange, loadingState: false }} />
-        </div>
-        {/* group members */}
-        <Box
-          flexGrow={'1'}
-          display={isMemberSelectionDone && forCreation ? 'none' : 'flex'}
-          flexDirection={'column'}
-          height={'60dvh'}
-          gap={'10px'}
-          marginTop={'10px'}
-          overflowY={'scroll'}
-        >
-          {chats &&
-            chats!.map((chat) => (
-              <GroupMemberCard key={chat._id} chatMembers={chat.members} setSelectedMembers={setSelectedMembers} />
-            ))}
-          {/* TODO: refactor this group member card props */}
-        </Box>
-        {/* Group name and avatar */}
-        <InputGroup display={isMemberSelectionDone && forCreation ? 'flex' : 'none'} alignItems={'center'} gap={'1.5rem'}>
-          <Box
-            backgroundColor={'gray.100'}
-            borderRadius={'50%'}
-            width={'fit-content'}
-            padding={'10px'}
-            position={'relative'}
-          >
-            <CiCamera size={'1.5rem'} />
-            <Input
-              type='file'
-              name='avatar'
-              accept='image/jpg,image/png'
-              className={styles.choose_group_avatar}
-              onChange={inputChangeHandler}
-            />
-          </Box>
-          <Input placeholder={t('chooseGroupName')} ringColor={'green.400'} onChange={inputChangeHandler} name='name' />
-        </InputGroup>
-        {/* countine btn */}
-        <Button
-          colorScheme='blue'
-          onClick={continueHandler}
-          isDisabled={isLoading}
-          paddingTop={'10px'}
-          paddingBottom={'10px'}
-        >
-          {isLoading ? <Spinner /> : addMembersAndContinueBtnText}
-        </Button>
-      </DrawerContent>
-    </Drawer>
+    <Drawer.Root
+      open={isCreateChatGroupMenuOpen}
+      onClose={() => dispatch(setVisablityOfCreateChatGroupMenu(false))}
+    >
+      <Drawer.Portal>
+        <Drawer.Overlay className="fixed inset-0 bg-black/40" />
+        <Drawer.Content className="bg-gray-100 h-[95dvh] fixed bottom-0 left-0 right-0 outline-none rounded-t-xl">
+          <div className="p-4 bg-white h-full rounded-t-xl flex flex-col gap-2">
+            <Drawer.Handle />
+            <div className="mt-2">
+              <SearchInput data={{ handleFormChange, loadingState: false }} />
+            </div>
+            {/* group members */}
+            <Box
+              flexGrow={'1'}
+              display={isMemberSelectionDone && forCreation ? 'none' : 'flex'}
+              flexDirection={'column'}
+              height={'60dvh'}
+              gap={'10px'}
+              marginTop={'10px'}
+              overflowY={'scroll'}
+            >
+              {chats &&
+                chats!.map((chat) => (
+                  <GroupMemberCard
+                    key={chat._id}
+                    chatMembers={chat.members}
+                    setSelectedMembers={setSelectedMembers}
+                  />
+                ))}
+              {/* TODO: refactor this group member card props */}
+            </Box>
+            {/* Group name and avatar */}
+            <InputGroup
+              display={isMemberSelectionDone && forCreation ? 'flex' : 'none'}
+              alignItems={'center'}
+              gap={'1.5rem'}
+            >
+              <Box
+                backgroundColor={'gray.100'}
+                borderRadius={'50%'}
+                width={'fit-content'}
+                padding={'10px'}
+                position={'relative'}
+              >
+                <CiCamera size={'1.5rem'} />
+                <Input
+                  type="file"
+                  name="avatar"
+                  accept="image/jpg,image/png"
+                  className={styles.choose_group_avatar}
+                  onChange={inputChangeHandler}
+                />
+              </Box>
+              <Input
+                placeholder={t('chooseGroupName')}
+                ringColor={'green.400'}
+                onChange={inputChangeHandler}
+                name="name"
+              />
+            </InputGroup>
+            {/* countine btn */}
+            <Button
+              colorScheme="blue"
+              onClick={continueHandler}
+              isDisabled={isLoading}
+              paddingTop={'10px'}
+              paddingBottom={'10px'}
+            >
+              {isLoading ? <Spinner /> : addMembersAndContinueBtnText}
+            </Button>
+          </div>
+        </Drawer.Content>
+      </Drawer.Portal>
+    </Drawer.Root>
   );
 };
 
