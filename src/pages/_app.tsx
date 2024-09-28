fff5f07823363edd1f30c193e3da9e93e965bac6
HosamAlden <hosam98j@gmail.com>
2024-09-28 10:48:13 +0200

    fix eslint errors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unknown-property */
+
 import { RootState, store, wrapper } from '@/redux/store';
 import React, { useState } from 'react';
 import '@/styles/globals.css';
