e3a3d5a24be0f30a3193f78ac3c857b5d1e3ad7f
HosamAlden <hosam98j@gmail.com>
2024-10-01 16:32:43 +0200

    voice-memo/bugs fixes, enhancments

--- src/components/VoiceMemoPlayer/index.tsx
+++ src/components/VoiceMemoPlayer/index.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/no-unknown-property */
 import React, { useEffect, useRef, useState } from 'react';
 import styles from './styles.module.scss';
-import { Box, Text } from '@chakra-ui/react';
 import { BsFillPauseFill, BsFillPlayFill } from 'react-icons/bs';
 import { secondsToDurationConverter } from '@/utils/time';
 import { ChatMessage } from '@/interfaces/chat.interface';
@@ -16,57 +15,87 @@ const VoiceMemoPlayer: React.FC<{ data: Props }> = ({ data }) => {
   // destruct props
   const { sender, content, voiceNoteDuration: duration } = data;
   // get data from redux store
-  const { currentUsr } = useSelector((state: RootState) => ({ currentUsr: state.auth.currentUser }));
+  const currentUser = useSelector((state: RootState) => state.auth.currentUser);
   // is voice note is sended by current loogedIn usr ?
-  const sendedByMe = sender._id === currentUsr?._id;
+  const sendedByMe = sender._id === currentUser?._id;
   // audio content or url
-  const [audioUrl] = useState(() => {
-    // check if content contain http
-    if (content.includes('data:')) return content;
-    // otherwize
-    return `${apiHost}${content}`;
-  });
+  const audioUrl = content.includes('data:') ? content : `${apiHost}${content}`;
   // is audio playing
   const [isAudioPlaying, setAudioPlaying] = useState(false);
+  // audio timer
+  let [timer, setTimer] = useState(0);
   // play and pause icon
   const PlayPauseIcon = isAudioPlaying ? BsFillPauseFill : BsFillPlayFill;
   // audio ref
   const audioRef = useRef<HTMLAudioElement>(null);
   // timeline ref
   const timeLineRef = useRef<HTMLSpanElement>(null);
-  // audio timer
-  let timer = 0;
-  // component mount
-  useEffect(() => {
-    audioRef.current?.load();
-  }, []);
+  // component mount, load audio element
+  useEffect(() => audioRef.current?.load(), []);
+  // interval
+  let [interval, resetInterval] = useState<NodeJS.Timer>(null!);
   // handle play audio
   const audioPlayHandler = () => {
-    timeLineRef.current?.setAttribute('style', 'width: 0%');
+    // audio playing status
+    const audioPlayingStatus = !isAudioPlaying;
+    // set audio play status
+    setAudioPlaying(audioPlayingStatus);
+    // timeline
+    const timeline = timeLineRef.current!;
+    // if audio is stopped
+    if (!audioPlayingStatus) {
+      clearInterval(interval);
+      audioRef.current?.pause();
+      return;
+    }
+    // play audio
     audioRef.current?.play();
-    setAudioPlaying(true);
-    const interval = setInterval(() => {
-      timer++;
-      timeLineRef.current?.setAttribute('style', `width:${(timer / Number(duration)) * 100}%`);
-      if (timer === Number(duration)) {
-        clearInterval(interval);
+    // repeate every 1 seconds
+    const setinterval = setInterval(() => {
+      // inc timer
+      setTimer(timer++);
+      // current timeline width
+      const timeLineProgress = (timer / Number(duration)) * 100;
+      // set time line progress
+      timeline.setAttribute(
+        'style',
+        `width:${timeLineProgress}%; transition: all 1s linear`,
+      );
+      // when audo playing is completed
+      if (timer !== Number(duration)) return;
+      // clear interval
+      clearInterval(setinterval);
+      // wait until transition end
+      timeline.addEventListener('transitionend', () => {
+        // reset styles
+        timeline.setAttribute('style', 'transition: none; width: 0%');
+        // set timer
+        setTimer(0);
+        // audio is stopped
         setAudioPlaying(false);
-      }
+      });
     }, 1000);
+    // make timer global
+    resetInterval(setinterval);
   };
   return (
     <div className={styles.voice_memo_rec} sended-by-me={String(sendedByMe)}>
+      {/* play / pause icon */}
       <PlayPauseIcon onClick={audioPlayHandler} size={'2rem'} />
-      <Box display={'flex'} flexDirection={'column'} gap={'4'}>
+      {/* player container */}
+      <div className="flex gap-2 items-center flex-grow">
+        {/* time line */}
         <div className={styles.time_line}>
           <span ref={timeLineRef} className={styles.time_line_filler}></span>
         </div>
-        <Text lineHeight={'0'} fontSize={'sm'} className={styles.timer} textColor={'gray'}>
+        {/* timer */}
+        <span className="text-sm bg-gray-200 px-1 rounded-lg">
           {secondsToDurationConverter(Number(duration))}
-        </Text>
-      </Box>
+        </span>
+      </div>
+      {/* audio element */}
       <audio ref={audioRef}>
-        <source src={audioUrl} type='audio/webm' />
+        <source src={audioUrl} type="audio/webm" />
       </audio>
     </div>
   );
--- src/utils/time.ts
+++ src/utils/time.ts
@@ -4,18 +4,56 @@ export enum TimeUnits {
   'fullTime' = 'fullTime',
 }
 
-export function getTime(stringDate: string, timeUnit: TimeUnits, local: never = 'en' as never) {
+export function getTime(
+  stringDate: string,
+  timeUnit: TimeUnits,
+  local: never = 'en' as never,
+) {
   // terminate if there is no date provided
   if (!stringDate) return '';
   // week's days
   const weekDays = {
-    ar: ['الأحد', 'الإثنين', 'الثلاثاء', 'الأربعاء', 'الخميس', 'الجمعة', 'السبت'],
+    ar: [
+      'الأحد',
+      'الإثنين',
+      'الثلاثاء',
+      'الأربعاء',
+      'الخميس',
+      'الجمعة',
+      'السبت',
+    ],
     en: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
   };
   // months
   const months = {
-    ar: ['يناير', 'فبراير', 'مارس', 'أبريل', 'مايو', 'يونيو', 'يوليو', 'أغسطس', 'سبتمبر', 'أكتوبر', 'نوفمبر', 'ديسمبر'],
-    en: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    ar: [
+      'يناير',
+      'فبراير',
+      'مارس',
+      'أبريل',
+      'مايو',
+      'يونيو',
+      'يوليو',
+      'أغسطس',
+      'سبتمبر',
+      'أكتوبر',
+      'نوفمبر',
+      'ديسمبر',
+    ],
+    en: [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ],
   };
   //create new date object from string
   const dateObj = new Date(stringDate);
@@ -34,7 +72,8 @@ export function getTime(stringDate: string, timeUnit: TimeUnits, local: never = 
   // if time unit is date
   if (timeUnit === TimeUnits.date) return `${dayOfWeek} ${dayOfMonth} ${month}`;
   // in case of full time
-  if (timeUnit === TimeUnits.fullTime) return `${dayOfWeek} ${dayOfMonth} ${month} ${timeFormater({ hours, minites })}`;
+  if (timeUnit === TimeUnits.fullTime)
+    return `${dayOfWeek} ${dayOfMonth} ${month} ${timeFormater({ hours, minites })}`;
 }
 
 // time formater
@@ -48,8 +87,11 @@ function timeFormater(data: { hours: string; minites: string }) {
 
 // convert seconds to duration
 export const secondsToDurationConverter = (sec: number) => {
-  const minites = sec >= 60 ? String(Math.round(sec / 60)) : '00';
+  let minites = sec >= 60 ? String(Math.round(sec / 60)) : '0';
   let seconds = sec >= 60 ? String(Math.round(sec % 60)) : Math.round(sec);
+  // formate minites
+  Number(minites) < 10 ? (minites = `0${minites}`) : '';
+  // formate seconds
   Number(seconds) < 10 ? (seconds = `0${seconds}`) : '';
   return `${minites}:${seconds}`;
 };
